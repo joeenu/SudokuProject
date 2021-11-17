6a0a983856364e20d30645d996bcecbfcf0b14a0
joeenu <jonasfasler@gmail.com>
2021-11-17 20:18:48 +0100

    added segmentpin values

--- Test_Multiplexing.py
+++ Test_Multiplexing.py
@@ -6,14 +6,14 @@ i2c = I2C(0,scl=Pin(1), sda=Pin(0))
 mcp = mcp23017.MCP23017(i2c, 0x20)
 
 # defining the pins each segment is connected to
-Seg0 = 00
-Seg1 = 00
-Seg2 = 00
-Seg3 = 00
-Seg4 = 00
-Seg5 = 00
-Seg6 = 00
-Seg7 = 00
+Seg0 = 8
+Seg1 = 9
+Seg2 = 10
+Seg3 = 11
+Seg4 = 12
+Seg5 = 13
+Seg6 = 14
+Seg7 = 15
 
 
 # selects the Layer with the case statement, then checks wether the current led needs to be turned on.
@@ -41,5 +41,4 @@ def WriteToSeg(SegValue, SegPin, ActiveLayer):
         case 7:
             if SegValue == 2 or 3 or 4 or 5 or 6 or 8 or 9:
                 mcp[SegPin].output(0)
-
 
