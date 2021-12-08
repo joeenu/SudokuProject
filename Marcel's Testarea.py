63d828a64e052aafc7eb1770385416a63b4be9a7
goenncel <brot.monster2002@gmail.com>
2021-12-08 13:09:25 +0100

    padis code

--- Marcel's Testarea.py
+++ Marcel's Testarea.py
@@ -1,4 +1,4 @@
-
+from sudoku_generator.Sudoku.Generator import *
 
 
 solvedlist = [  "1","2","3","4","5","6","7","8","9",
@@ -10,11 +10,11 @@ solvedlist = [  "1","2","3","4","5","6","7","8","9",
                 "4","5","6","7","8","9","1","2","3",
                 "3","4","5","6","7","8","9","1","2",
                 "2","3","4","5","6","7","8","9","1"]
-print(solvedlist)
+
+#print(solvedlist)
 
 
-
-Specifylist = [ "1"," ","3","4","5","6","7","8","9",
+specifylist = [ "1","","3","4","5","6","7","8","9",
                 "9","1","2","3","4","5","6","7","8",
                 "8","9","1","2","3","4","5","6","7",
                 "7","8","9","1","2","3","4","5","6",
@@ -24,11 +24,10 @@ Specifylist = [ "1"," ","3","4","5","6","7","8","9",
                 "3","4","5","6","7","8","9","1","2",
                 "2","3","4","5","6","7","8","9","1"]
 
-print(Specifylist)
+#print(specifylist)
 
 
-
-Workinglist = [ "1"," ","3","4","5","6","7","8","9",
+workinglist = [ "1","","3","4","5","6","7","8","9",
                 "9","1","2","3","4","5","6","7","8",
                 "8","9","1","2","3","4","5","6","7",
                 "7","8","9","1","2","3","4","5","6",
@@ -38,7 +37,58 @@ Workinglist = [ "1"," ","3","4","5","6","7","8","9",
                 "3","4","5","6","7","8","9","1","2",
                 "2","3","4","5","6","7","8","9","1"]
 
-print(Workinglist)
 
-Workinglist(22) = 5
 
+segmentbutton = 1
+
+numpadinput = 15
+
+lastnumpad = 39
+
+if len(specifylist[segmentbutton]) == 0:
+    if numpadinput != 0:                            #funktioniert ned wenn s feld 3 bsp leer isch schribts dete nüt ine
+        workinglist[segmentbutton] = numpadinput
+        numpadinput = 0
+    
+
+print(workinglist)
+
+
+
+
+
+difficulty = 40, 5 #da müend zwei variable ine wod schwirigkeit bestimmed. die 1. isch zum für schwirigkeitsgrad (höcher = schwieriger, 0-81) die zweit isch random schwirigkeit (ca 0 - 15)
+
+gen = Generator("C:\\Users\\marce\\OneDrive\\Dokumente\\Code\\SudokuProject\\sudoku_generator\\base.txt")
+
+gen.randomize(100)
+
+initial = gen.board.copy()
+
+gen.reduce_via_logical(difficulty[0])
+
+if difficulty[1] != 0:
+    
+    gen.reduce_via_random(difficulty[1])
+
+
+final = gen.board.copy()
+
+print("The initial board before removals was: \r\n\r\n{0}".format(initial))
+print("The generated board after removals was: \r\n\r\n{0}".format(final))
+print("")
+
+
+
+final_string = format(final)
+
+final_list = []
+for i in final_string:
+    if i == "_":
+        final_list.append(0)
+    if i == "|":
+        break
+    else:
+        final_list.append(i)
+
+print(final_list)
